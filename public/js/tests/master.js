a88da420a569c05962aca76f44d93f2fa4b65445
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-18 13:26:10 -0500

    added tests re: reserved seats

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -24,3 +24,35 @@ test('seat generation', function(){
   deepEqual($('#genAdmission div').length, 30, 'there are 30 genAdmission seats');
   deepEqual($('#genAdmission').data('cost'), '$50.00', 'genAdmission seats are $110');
 });
+
+test('reserve seats', function(){
+  expect(4);
+  $('#section').val('#genAdmission');
+  $('#amount').val('30');
+  $('#cost').val('$50');
+  $('#createButton').trigger('click');
+  $('#section').val('#vip');
+  $('#amount').val('15');
+  $('#cost').val('$110');
+  $('#createButton').trigger('click');
+
+  $('#name').val('Chyld');
+  $('#vip div:nth-child(5)').trigger('dblclick');
+
+  deepEqual($('#vip div:nth-child(5)').text(), 'Chyld', 'double clicked to reserve seat for Chyld');
+
+  $('#name').val('Bill');
+  $('#genAdmission div:nth-child(5)').trigger('dblclick');
+
+  deepEqual($('#genAdmission div:nth-child(5)').text(), 'Bill', 'double clicked to reserve seat for Bill');
+
+  $('#name').val('Andy');
+  $('#vip div:nth-child(5)').trigger('dblclick');
+
+  deepEqual($('#vip div:nth-child(5)').text(), 'Chyld', 'seat remained reserved');
+
+  $('#name').val('Fritz');
+  $('#genAdmission').trigger('dblclick');
+
+  deepEqual($('#vip div:nth-child(5)').text(), 'Bill', 'seat remained reserved');
+});
