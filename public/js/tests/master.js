ece5d534f418e315e93f553b7e3a63a08239bf88
Mike Kunkel <yourmom.tv@gmail.com>
2013-10-18 11:43:29 -0500

    create seats test

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -1,14 +1,27 @@
-// 'use strict';
+'use strict';
 
-// module('Integration Testing', {setup: setupTest, teardown: teardownTest});
+module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
-// function setupTest(){
-//   initialize(null, true);
-// }
+function setupTest(){
+  initialize(null, true);
+}
 
-// function teardownTest(){
-// }
+function teardownTest(){
+}
 
-// test('<name-of-feature>', function(){
-//   expect(1);
-// });
+test('seat generation', function(){
+  expect(4);
+  $('#section').val('genAdmission');
+  $('#amount').val('30');
+  $('#cost').val('$50');
+  $('#createButton').trigger('click');
+  $('#section').val('vip');
+  $('#amount').val('15');
+  $('#cost').val('$110');
+  $('#createButton').trigger('click');
+
+  deepEqual($('#vip div').length, 15, 'there are 15 vip seats');
+  deepEqual($('#vip').data('price'), '$110', 'vip seats are $110');
+  deepEqual($('#genAdmission div').length, 15, 'there are 30 genAdmission seats');
+  deepEqual($('#genAdmission').data('price'), '$50', 'genAdmission seats are $110');
+});
