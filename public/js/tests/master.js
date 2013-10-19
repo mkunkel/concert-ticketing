d3e869f4976d4b187292cc02f68632bb4cb8ea37
Benjamin Blackburn <benjamin.blackburn@gmail.com>
2013-10-19 15:36:42 -0500

    finished testing on reporting and seat list

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -38,21 +38,54 @@ test('reserve seats', function(){
   $('#createButton').trigger('click');
 
   $('#name').val('Chyld');
-  $('#vip div:nth-child(5)').trigger('click');
+  $('#vip div:nth-child(5)').trigger('dblclick');
   deepEqual($('#vip div:nth-child(5) .resName').text(), 'Chyld', 'double clicked to reserve seat for Chyld');
 
   $('#name').val('Bill');
-  $('#genAdmission div:nth-child(5)').trigger('click');
+  $('#genAdmission div:nth-child(5)').trigger('dblclick');
 
   deepEqual($('#genAdmission div:nth-child(5) .resName').text(), 'Bill', 'double clicked to reserve seat for Bill');
 
   $('#name').val('Andy');
-  $('#vip div:nth-child(5)').trigger('click');
+  $('#vip div:nth-child(5)').trigger('dblclick');
 
   deepEqual($('#vip div:nth-child(5) .resName').text(), 'Chyld', 'seat remained reserved');
 
   $('#name').val('Fritz');
-  $('#genAdmission div:nth-child(5)').trigger('click');
+  $('#genAdmission div:nth-child(5)').trigger('dblclick');
 
   deepEqual($('#genAdmission div:nth-child(5) .resName').text(), 'Bill', 'seat remained reserved');
+});
+
+test('create reporting', function(){
+  expect(8);
+  $('#section').val('#genAdmission');
+  $('#amount').val('30');
+  $('#cost').val('$50');
+  $('#createButton').trigger('click');
+  $('#section').val('#vip');
+  $('#amount').val('15');
+  $('#cost').val('$110');
+  $('#createButton').trigger('click');
+
+  $('#name').val('Chyld');
+  $('#vip div:nth-child(4)').trigger('dblclick');
+
+  $('#name').val('Bill');
+  $('#genAdmission div:nth-child(4)').trigger('dblclick');
+
+  $('#name').val('Andy');
+  $('#vip div:nth-child(6)').trigger('dblclick');
+
+  $('#name').val('Fritz');
+  $('#genAdmission div:nth-child(6)').trigger('dblclick');
+
+  deepEqual($('#left #vipTotal').text(), '$220.00', 'total VIP cost');
+  deepEqual($('#left #genAdmissionTotal').text(), '$100.00', 'total GA cost');
+  deepEqual($('#left #grandTotal').text(), '$320.00', 'total VIP and GA cost');
+  deepEqual($('#left #vipPeople').text(), '2', 'total VIP tickets sold');
+  deepEqual($('#left #genAdmissionPeople').text(), '2', 'total GA tickets sold');
+  deepEqual($('#left #totalPeople').text(), '4', 'total VIP and GA tickets sold');
+  deepEqual($('#right > #listVip .peopleList').text(), 'V3 - Andy', 'Andy is in V3');
+  deepEqual($('#right > #listGenAdmission .peopleList').text(), 'G3 - Hank', 'Hank is in G3');
 });
